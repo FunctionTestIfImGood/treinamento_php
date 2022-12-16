20e9cdf259655872cf805ae2837f2bb1c64421c6
iago holek <87425168+FunctionTestIfImGood@users.noreply.github.com>
2022-12-16 00:02:27 -0300

    Commit inicial
    Treinando alguns principios da programação no PHP

---
+++ src/units/cliente.php
@@ -0,0 +1,22 @@
+<?php
+class Cliente
+{
+ public function __construct(public string $nome, public string $endereco, public readonly int $idade) 
+ {
+ }
+
+ public function validateName() {
+    if ($this->nome == "iago") {
+         echo "lindao\n";
+    }
+}
+ public function validateAge() {
+    if ($this->idade == 24) {
+        echo "ain pai para\n";
+    }
+ }
+}
+$cliente = new Cliente("iago", "rua zap zap", 24);
+$cliente->validateName();
+$cliente->validateAge();
+print($cliente->nome . " / " . $cliente->endereco . " / " . $cliente->idade);
